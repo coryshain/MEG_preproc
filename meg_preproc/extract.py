34a6df60cbae14b9a66830b4c50847fb60b2d767
coryshain <cory.shain@gmail.com>
2022-01-31 13:45:44 -0500

    Updates to extraction script and docs

--- meg_preproc/extract.py
+++ meg_preproc/extract.py
@@ -17,10 +17,13 @@ if __name__ == '__main__':
     Extract tabular event-related representation of cleaned MEG timecourses
     ''')
     argparser.add_argument('config', help='Path to config file containing extraction instructions.')
-    argparser.add_argument('-j', '--n_jobs', default='cuda', help='Number of jobs for parallel processing, or "cuda" to attempt GPU acceleration if available.')
-    argparser.add_argument('-d', '--debug', action='store_true', help='Run in debug mode on a subset of data for speed, saving plots but not data.')
+    argparser.add_argument('-j', '--n_jobs', default='cuda', help=(
+        'Number of jobs for parallel processing, or "cuda" to attempt GPU acceleration if available.'
+    ))
+    argparser.add_argument('-d', '--debug', action='store_true', help=(
+        'Run in debug mode on a subset of data for speed, saving plots but not data.'
+    ))
     args = argparser.parse_args()
-
 
     # Parse config
     debug = args.debug
@@ -44,6 +47,7 @@ if __name__ == '__main__':
     config_path = args.config
     with open(config_path, 'r') as f:
         config = yaml.load(f, Loader=Loader)
+    expt_name = config['expt_name']
     subjects = config['subjects']
     outdir = config.get('outdir', './')
     clean_code = config.get('clean_code', 'default_meg')
@@ -59,7 +63,7 @@ if __name__ == '__main__':
     else:
         event_name_to_code = {}
     epoch_tmin = config.get('epoch_tmin', -0.2)
-    epoch_tmax = config.get('epoch_tmax', 1.0)
+    epoch_tmax = config.get('epoch_tmax', 0.8)
     end_code = config.get('end_code', None)
     end_key = config.get('end_key', None)
     if end_code is not None or end_key is not None:
@@ -74,6 +78,16 @@ if __name__ == '__main__':
             end_key = event_code_to_name[end_code]
     else:
         epoch_data = True
+    word_level_file = config.get('word_level_file', None)
+    if word_level_file:
+        word_level_events = pd.read_csv('word_level_file')
+        assert 'condition' in word_level_events and 'word_onset_time' in word_level_events, (
+                'File %s was provided via field ``word_level_file``, but it is not correctly formatted.'
+                ' It must contain columns called "condition" and "word_onset_time" to enable alignment'
+                ' to the events file.'
+        ) % word_level_file
+    else:
+        word_level_events = None
 
     reject = dict(mag=8000e-15, grad=8000e-13)  # T, T/m
     flat = dict(mag=1e-15, grad=1e-13)  # T/m
@@ -87,20 +101,42 @@ if __name__ == '__main__':
 
         # Load data
         info('Processing subject directory: %s' % subject_dir, marker='*')
+        protocol_path = os.path.join(subject_dir, 'protocol.txt')
+        assert os.path.exists(protocol_path), (
+                'No protocol file ("protocol.txt", comma-delimited list of expertiment_name,start_time) found in'
+                ' directory %s.'
+        ) % subject_dir
+        with open(protocol_path) as f:
+            expt_intervals = {}
+            lines = list(f.readlines())
+            for i, l in enumerate(lines):
+                name, start = l.split(',')
+                if i < len(lines) - 1:
+                    end = float(lines[i+1].split(',')[1].strip())
+                else:
+                    end = None
+                expt_intervals[name.strip()] = (float(start.strip()), end)
+        assert expt_name in expt_intervals, (
+                'Experiment name %s not found in protocol file, which had experiment names %s.'
+        ) % (expt_name, ', '.join(expt_intervals.keys()))
+        expt_start, expt_end = expt_intervals[expt_name]
         fifs = [x for x in os.listdir(subject_dir) if x.endswith('%s.fif' % clean_code)]
         if not fifs:
             stderr('No *_cleaned_meg.fif files found in directory %s. Skipping...\n' % subject_dir)
             continue
         assert len(fifs) == 1, 'Found multiple matching files in directory %s: %s.' % (subject_dir, ', '.join(fifs))
         fif_base = os.path.join(subject_dir, fifs[0])
-        print(fif_base)
         if not os.path.exists(fif_base):
             stderr('The *.fif files in directory %s are not correctly named. Skipping...\n' % subject_dir)
             continue
-        if debug:
-            raw = mne.io.Raw(fif_base).crop(tmax=1000)
+        raw = mne.io.Raw(fif_base)
+        if expt_end:
+            expt_end = min(expt_end, raw.times.max())
         else:
-            raw = mne.io.Raw(fif_base)
+            expt_end = raw.times.max()
+        if debug:
+            expt_end = min(expt_end, expt_start + 1000)
+        raw.crop(tmin=expt_start, tmax=expt_end)
 
         # Load events
         all_events = mne.find_events(raw, stim_channel='STI101', min_duration=0.002)
@@ -172,8 +208,10 @@ if __name__ == '__main__':
                         assert _event_code is not None, "Finished with invalid event code at sample %s." % t
                         assert _event_start is not None, "Finished with invalid event start time at sample %s." % t
 
+                        _event_end = t
+
                         start = min(_event_start * time_scale + epoch_tmin, data.times.max())
-                        end = min(t * time_scale + epoch_tmax, data.times.max())
+                        end = min(_event_end * time_scale + epoch_tmax, data.times.max())
 
                         _data = data.copy().crop(start, end)
 
@@ -191,7 +229,8 @@ if __name__ == '__main__':
                             'subject': [os.path.basename(subject_dir)],
                             'epoch': [epoch_ix],
                             'condition': [event_code_to_name[_event_code]],
-                            'onset_time': [_event_start]
+                            'onset_time': [_event_start],
+                            'offset_time': [_event_end]
                         })
                         events.append(_events)
 
@@ -201,16 +240,15 @@ if __name__ == '__main__':
                         _event_end = None
                         epoch_ix += 1
 
-    exit()
-
     if responses:
         info('Saving response table')
         responses = pd.concat(responses, axis=0)
+        responses = responses.reset_index()
         responses['time'] = responses['time'] * time_scale
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         responses.to_csv(
-            os.path.join(outdir, os.path.basename(config_path[:-4]) + '_responses.csv'),
+            os.path.join(outdir, os.path.basename(expt_name) + '_responses.csv'),
             index=False
         )
     else:
@@ -218,12 +256,22 @@ if __name__ == '__main__':
 
     if events:
         info('Saving response table')
-        events = pd.concat(events, axis=0)
-        events['time'] = events['time'] * time_scale
+        events = pd.concat(events, axis=0).reset_index(drop=True)
+        events['onset_time'] = events['onset_time'] * time_scale
+        if 'offset_time' in events:
+            events['offset_time'] = events['offset_time'] * time_scale
+            events['duration'] = events['offset_time'] - events['onset_time']
         if not os.path.exists(outdir):
             os.makedirs(outdir)
+        if word_level_events is not None:
+            events = pd.merge(events, word_level_events, on=['condition'])
+            events.word_onset_time = events.word_onset_time + events.onset_time
+            if 'word_offset_time' in events:
+                events.word_offset_time = events.word_offset_time + events.onset_time
+                events.word_duration = events.word_offset_time - events.word_onset_time
+
         events.to_csv(
-            os.path.join(outdir, os.path.basename(config_path[:-4]) + '_events.csv'),
+            os.path.join(outdir, os.path.basename(expt_name) + '_events.csv'),
             index=False
         )
     else:
